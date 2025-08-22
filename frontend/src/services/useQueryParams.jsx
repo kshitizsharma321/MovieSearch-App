b5bc202a7d2a00138f5101b96d5a030f028c5090
Kshitiz Sharma <kshitizsharma@MacBookAir.local>
2025-08-22 18:37:13 +0530

    Used memo in this custom hook

--- frontend/src/services/useQueryParams.jsx
+++ frontend/src/services/useQueryParams.jsx
@@ -1,11 +1,17 @@
 import { useLocation, useSearchParams, useNavigate } from 'react-router';
 
+import { useMemo } from 'react';
+
 export function useQueryParams() {
 	const navigate = useNavigate();
 	let { pathname } = useLocation();
 	const [searchParams] = useSearchParams();
-	const name = searchParams.get('q') || undefined;
-	const media = searchParams.get('type') || 'movie';
-	const page = searchParams.get('page') || 1;
-	return { navigate, pathname, name, media, page };
+
+	const queryData = useMemo(() => {
+		const name = searchParams.get('q') || undefined;
+		const media = searchParams.get('type') || 'movie';
+		const page = searchParams.get('page') || 1;
+		return { navigate, pathname, name, media, page };
+	}, [navigate, pathname, searchParams]);
+	return queryData;
 }
